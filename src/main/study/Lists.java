a17c288a776de895bd8925e0f08fff9b1f4f43dd
alkekkonen <alexander.kekkonen@gmail.com>
2017-11-29 11:56:44 +0300

    Lists and HashMap.

--- src/main/study/Lists.java
+++ src/main/study/Lists.java
@@ -3,12 +3,16 @@ package study;
 import java.util.*;
 
 
-public class Study {
+public class Lists {
 
     public static void main(String[] args) {
 
-        //Объявляем массив, использця полиморфизм (делаем List стрингов с атрибутами его наследника ArrayList)
+//        Объявляем коллекцию, использця полиморфизм (делаем List стрингов со свойствами его наследника ArrayList)
+//        При объявлении указываем тип объектов коллекции
         List<String> myList = new ArrayList<>();
+
+        //Объявляем LinkedList
+        List<String> myLinkedList = new LinkedList<>();
 
         //Добавляем в лист элементы по имени
         myList.add("Котик");
@@ -18,7 +22,7 @@ public class Study {
 
         //Используем реоегруженный метод add() — делаем вставку по индексу в конкретное место
         // (вставляем значение на место второго элемента, у всех последующих элементов увеличивается индекс)
-        myList.add(1, "Слоник");
+        myList.add(1, "Зелёный слоник");
 
         //заменяем элемент с указанным индексом
         myList.set(4, "Улиточка Даниил");
@@ -26,39 +30,17 @@ public class Study {
         //Достаём элемент по индексу
         myList.get(3);
 
+        //Достаём индекс элемента
+        myList.indexOf("Червячок Кирилл");
+
         //Проверяем, есть ли в списке значение (true/false)
-        myList.contains("Слоник");
+        myList.contains("Зелёный слоник");
 
+        //Узнаём размер списка
+        myList.size();
 
-        //Очищаем список
+        //Очищаем лист
         myList.clear();
-
-        System.out.println(myList);
-
-//
-//        Random random = new Random();
-//
-//        System.out.println(random.nextInt(100));
-
-//        Map<Integer, String> map = new TreeMap<>();
-////        map.put(random.nextInt(100), "Котик");
-////        map.put(null, "Пёсик");
-////        map.put(3, null);
-////        map.put(4, "Червячок Кирилл");
-//        map.get(random.nextInt(20));
-//
-//        System.out.println(map.size());
-
-//        System.out.println(map.containsKey(4));
-
-
-//        System.out.println(map);
-
-
-//        System.out.println(myList.size());
-//
-//
-//        System.out.println(myList);
 
 
     }
